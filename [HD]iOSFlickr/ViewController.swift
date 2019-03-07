ce02e4ac16bd3d4fb3bf2ac24419358edc2aca53
Хизир Дудуев <hizir.duduev@gmail.com>
2019-03-07 13:58:38 +0300

    Initial Commit

---
+++ [HD]iOSFlickr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  [HD]iOSFlickr
+//
+//  Created by -=HIZIR=- on 07/03/2019.
+//  Copyright © 2019 GurobaDeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
